46686c6b21c83ba9807f0724b89af24586366d43
urd00m <alan.linghao.wang@gmail.com>
2021-06-24 15:45:59 -0400

    Files for the better model rostalker_server and rostalker_client, this will implement the R1-R2-R3 model

---
+++ src/sync_robot_test/scripts/rostalker_client.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import rospy
+import time
+import sys
+from sync_robot_test.srv import *
+from sync_robot_test.msg import *
+from threading import Thread, Lock
+
+
+
+
+
+if __name__ == '__main__':
+	pass
---
+++ src/sync_robot_test/scripts/rostalker_server.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import rospy
+import sys
+import time
+from sync_robot_test.srv import *
+from sync_robot_test.msg import *
+from threading import Thread, Lock
+
+
+
+
+if __name__ == '__main__':
+	pass
